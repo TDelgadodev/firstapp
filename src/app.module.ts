97c4b15ea18194f4435c5228cddb664f4e41bcf7
Thiago <thiagodelgado.dev@gmail.com>
2024-12-08 18:24:28 -0300

    Add guards and pipes

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { TaskModule } from './tasks/task.module';
 import { ProjectsModule } from './projects/projects.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { HelloModule } from './hello/hello.module';
+import { PrismaService } from './prisma/prisma.service';
+import { PostController } from './post/post.controller';
 
 
 @Module({
-  imports: [TaskModule, ProjectsModule, AuthModule, UsersModule],
+  imports: [TaskModule, ProjectsModule, AuthModule, UsersModule, HelloModule],
+  providers: [PrismaService],
+  controllers: [PostController],
 })
 export class AppModule {}
